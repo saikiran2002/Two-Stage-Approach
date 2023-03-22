a47019e6e37027822a897acfddb76366e7cd310a
Naga Datha Saikiran Battula <55631075+saikiran2002@users.noreply.github.com>
2023-03-22 18:03:12 +0530

    Uploading Code files

---
+++ Two-Stage-AD(Stage-2).py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import os
+from distutils.dir_util import copy_tree, remove_tree
+
+from PIL import Image
+from random import randint
+
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import matthews_corrcoef as MCC
+from sklearn.metrics import balanced_accuracy_score as BAS
+from sklearn.metrics import classification_report, confusion_matrix
+
+import tensorflow_addons as tfa
+from keras.utils.vis_utils import plot_model
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Conv2D, Flatten
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing.image import ImageDataGenerator as IDG
+from tensorflow.keras.layers import SeparableConv2D, BatchNormalization, MaxPool2D
+from utils import Utils
+
+
+base_dir = "data/ADNI/"
+root_dir = "./"
+test_dir = base_dir + "test/"
+train_dir = base_dir + "train/"
+work_dir = root_dir + "dataset/"
+
+if os.path.exists(work_dir):
+    remove_tree(work_dir)
+
+os.mkdir("./models/")
+os.mkdir(work_dir)
+copy_tree(train_dir, work_dir)
+copy_tree(test_dir, work_dir)
+print("Working Directory Contents:", os.listdir(work_dir))
+
+WORK_DIR = './dataset/'
+
+CLASSES = os.listdir(work_dir)
+
+IMG_SIZE = 224
+IMAGE_SIZE = [224, 224]
+DIM = (IMG_SIZE, IMG_SIZE)
+
+ZOOM = [.99, 1.01]
+BRIGHT_RANGE = [0.8, 1.2]
+HORZ_FLIP = True
+FILL_MODE = "constant"
+DATA_FORMAT = "channels_last"
+
+work_dr = IDG(rescale = 1./255, brightness_range=BRIGHT_RANGE, zoom_range=ZOOM, data_format=DATA_FORMAT, fill_mode=FILL_MODE, horizontal_flip=HORZ_FLIP)
+
+train_data_gen = work_dr.flow_from_directory(directory=WORK_DIR, target_size=DIM, batch_size=1024, shuffle=True)
+
+# OverSampling
+train_data, train_labels = train_data_gen.next()
+sm = SMOTE(sampling_strategy="auto")
+train_data, train_labels = sm.fit_resample(train_data.reshape(-1, IMG_SIZE * IMG_SIZE * 3), train_labels)
+train_data = train_data.reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+
+train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
+train_data, val_data, train_labels, val_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('val_acc') > 0.99:
+            print("\nReached accuracy threshold! Terminating training.")
+            self.model.stop_training = True
+            
+my_callback = MyCallback()
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+
+# Model
+rm = Sequential()
+model = tf.keras.applications.resnet_v2.ResNet50V2(include_top=False,input_shape=(224,224,3),pooling='avg',classes=len(CLASSES),weights='imagenet')
+for layer in model.layers:
+    layer.trainable=False
+rm.add(model)
+rm.add(Flatten())
+rm.add(Dense(512,activation='relu'))
+rm.add(Dense(len(CLASSES),activation='softmax'))
+opt = tf.keras.optimizers.Adam()
+METRICS = [tf.keras.metrics.CategoricalAccuracy(name='acc'),
+           tf.keras.metrics.AUC(name='auc'), 
+           tfa.metrics.F1Score(num_classes=len(CLASSES))]
+CALLBACKS = [my_callback]
+rm.compile(optimizer=opt,loss=tf.losses.CategoricalCrossentropy(),metrics=METRICS)
+
+# Training
+EPOCHS = 75
+history = rm.fit(train_data_gen,validation_data=(val_data,val_labels), callbacks=CALLBACKS, epochs=EPOCHS)
+
+test_scores = rm.evaluate(test_data, test_labels)
+print("Testing Accuracy: %.2f%%"%(test_scores[1] * 100))
+pred_labels = rm.predict(test_data)
+
+ut = Utils(history,pred_labels,test_labels,CLASSES)
+
+ut.plot_ROC_curves()
+
+ut.Classification_report()
+
+ut.Confusion_matrix(title="Alzheimers\'s Disease Severity Diagnosis")
+
+ut.balanced_accuracy_score()
+ut.Mathews_correlation_coefficient()
+
+model_dir = "./models/" + "Alzheimer's_Disease_Severity_Diagnosis"
+rm.save(model_dir, save_format='h5')
+os.listdir(work_dir)
+
+# # Load Model
+# pretrained_model = tf.keras.models.load_model(model_dir)
+# plot_model(pretrained_model, to_file=work_dir + "model_plot.png", show_shapes=True, show_layer_names=True)
---
+++ Two-Stage-Stage-1.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import os
+from distutils.dir_util import copy_tree, remove_tree
+
+from PIL import Image
+from random import randint
+
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import matthews_corrcoef as MCC
+from sklearn.metrics import balanced_accuracy_score as BAS
+from sklearn.metrics import classification_report, confusion_matrix
+
+import tensorflow_addons as tfa
+from keras.utils.vis_utils import plot_model
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Conv2D, Flatten
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing.image import ImageDataGenerator as IDG
+from tensorflow.keras.layers import SeparableConv2D, BatchNormalization, MaxPool2D
+from utils import Utils
+
+
+base_dir = "data/"
+root_dir = "./"
+test_dir = base_dir + "test/"
+train_dir = base_dir + "train/"
+work_dir = root_dir + "dataset/"
+
+if os.path.exists(work_dir):
+    remove_tree(work_dir)
+
+os.mkdir("./models/")
+os.mkdir(work_dir)
+copy_tree(train_dir, work_dir)
+copy_tree(test_dir, work_dir)
+print("Working Directory Contents:", os.listdir(work_dir))
+
+WORK_DIR = './dataset/'
+
+CLASSES = os.listdir(work_dir)
+
+IMG_SIZE = 224
+IMAGE_SIZE = [224, 224]
+DIM = (IMG_SIZE, IMG_SIZE)
+
+ZOOM = [.99, 1.01]
+BRIGHT_RANGE = [0.8, 1.2]
+HORZ_FLIP = True
+FILL_MODE = "constant"
+DATA_FORMAT = "channels_last"
+
+work_dr = IDG(rescale = 1./255, brightness_range=BRIGHT_RANGE, zoom_range=ZOOM, data_format=DATA_FORMAT, fill_mode=FILL_MODE, horizontal_flip=HORZ_FLIP)
+
+train_data_gen = work_dr.flow_from_directory(directory=WORK_DIR, target_size=DIM, batch_size=1024, shuffle=True)
+
+# OverSampling
+train_data, train_labels = train_data_gen.next()
+sm = SMOTE(sampling_strategy="auto")
+train_data, train_labels = sm.fit_resample(train_data.reshape(-1, IMG_SIZE * IMG_SIZE * 3), train_labels)
+train_data = train_data.reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+
+train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
+train_data, val_data, train_labels, val_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('val_acc') > 0.99:
+            print("\nReached accuracy threshold! Terminating training.")
+            self.model.stop_training = True
+            
+my_callback = MyCallback()
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+
+# Model
+def construct_alexnet_model(act='relu'):
+    model = Sequential([
+        Input(shape=(*IMAGE_SIZE, 3)),
+        Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Flatten(),
+        Dense(4096, activation='relu'),
+        Dropout(0.5),
+        Dense(4096, activation='relu'),
+        Dropout(0.5),
+        Dense(len(CLASSES), activation='softmax')
+    ], name = "alexnet_cnn_model")
+
+    return model
+opt = tf.keras.optimizers.Adam()
+model = construct_alexnet_model()
+
+METRICS = [tf.keras.metrics.CategoricalAccuracy(name='acc'),
+           tf.keras.metrics.AUC(name='auc'), 
+           tfa.metrics.F1Score(num_classes=len(CLASSES))]
+
+CALLBACKS = [my_callback]
+    
+model.compile(optimizer=opt,
+              loss=tf.losses.CategoricalCrossentropy(),
+              metrics=METRICS)
+
+model.summary()
+
+# Training
+EPOCHS = 100
+history = model.fit(train_data_gen, validation_data=(val_data, val_labels), callbacks=CALLBACKS, epochs=EPOCHS)
+
+test_scores = model.evaluate(test_data, test_labels)
+print("Testing Accuracy: %.2f%%"%(test_scores[1] * 100))
+pred_labels = model.predict(test_data)
+
+ut = Utils(history,pred_labels,test_labels,CLASSES)
+
+ut.plot_ROC_curves()
+
+ut.Classification_report()
+
+ut.Confusion_matrix(title="Stage-1 Classification")
+
+ut.balanced_accuracy_score()
+ut.Mathews_correlation_coefficient()
+
+model_dir = "./models/" + "Stage_1_cnn_model"
+model.save(model_dir, save_format='h5')
+os.listdir(work_dir)
+
+# # Load model
+# pretrained_model = tf.keras.models.load_model(model_dir)
+# plot_model(pretrained_model, to_file=work_dir + "model_plot.png", show_shapes=True, show_layer_names=True)
